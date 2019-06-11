5422da9ec76812dc17338b0b14e1d6eeab8cc75c
brb45 <sunusd@yahoo.com>
2019-06-11 11:22:03 -0700

    6/11/2019 Dft1

--- log_test_std.py
+++ log_test_std.py
@@ -11,6 +11,7 @@ def print_iterator(it):
 #299
 # aa = "abcd"
 # print(set(aa))
+
 def getHint(secret, guess):
     dic_s, dic_g = {}, {}
     cnt_c, cnt_b = 0, 0
@@ -29,6 +30,37 @@ def getHint(secret, guess):
 
 
 print(getHint("1123",'0111'))
+
+def run_flow1(run_dir, flow_to_test, result_dir):
+    os.chdir(run_dir)
+    for flow_name in flow_to_test:
+        # Reset DUT
+        os.system("plink.exe -ssh -pw brcm1234 root@192.168.100.31 cd /root/Desktop/4378_FW/4378_18_10_336_REF; ./load_drv.sh;")
+        time.sleep(40)
+        # Remove old folders: Log and Result_LP before new test
+        if os.path.isdir("Log"):
+            shutil.rmtree("Log")
+        if os.path.isdir("Result_LP"):
+            shutil.rmtree("Result_LP")
+        while True:
+            if os.path.isdir("Log") or os.path.isdir("Result_LP"):
+                time.wait(1)
+            else:
+                break
+        run_cmd = ["IQfactRun_Console.exe", '-run', flow_name, "-repeat", "1", "-exit"]
+        p = subprocess.Popen(run_cmd)
+        if p.wait() != 0:
+            print("RUN time error")
+        test_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        test_time = "_".join(test_time.split())
+        log_dir = os.path.join(result_dir, os.path.splitext(flow_name)[0], test_time)
+
+        shutil.copytree("Log", os.path.join(log_dir, "Log"))
+        shutil.copytree("Result_LP", os.path.join(log_dir, "Result_LP"))
+        time.sleep(1) #TreeNode
+
+
+st = "qa test on package 4.40.0.23"
 
 
 def run_flow(run_dir, flow_to_test, result_dir):
