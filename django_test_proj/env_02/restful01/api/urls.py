8e90a7eead4fb61eee10c22840efbe91e5b42e1b
brb45 <sunusd@yahoo.com>
2019-07-01 13:38:42 -0700

    7/1/2019 dft-1

--- django_test_proj/env_02/restful01/api/urls.py
+++ django_test_proj/env_02/restful01/api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 #from .views import HelloWorldView
-from .views import SubscriberView
+from .views import SubscriberViewSet
 
 # Building REST APIs with plain old Django is very possible but we have to do a lot.
 urlpatterns = [
     #url(r'hello', hello_world, name="hello_world")  #http://localhost:8000/api/hello
-    url("subscriber", SubscriberView.as_view(), name="Subscripber")
+    url("subscriber", SubscriberViewSet.as_view({'get': 'list', 'post': 'create'})),
+    url("subscriber/^[0-9]$", SubscriberViewSet.as_view({'get': 'retrieve', 'put': 'update'}))
+    # SubscriberViewSet.as_view({'get': 'list', 'post': 'create'})  # For: /api/subscriber
+    # SubscriberViewSet.as_view({'get': 'retrieve', 'put': 'update'})  # For: /api/subscriber/1
 ]
--- django_test_proj/env_02/restful01/api/views.py
+++ django_test_proj/env_02/restful01/api/views.py
@@ -1,15 +1,15 @@
-# ListCreateAPIView function as both ListAPIView and CreateAPIView for GET and POST
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView\
-    #, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+# ModelViewSet
+
+from rest_framework.viewsets import ModelViewSet
 
 from .serializers import SubscriberSerializer
 from .models import Subscriber
 
-class SubscriberView(ListCreateAPIView):
+class SubscriberViewSet(ModelViewSet):
     serializer_class = SubscriberSerializer
     queryset = Subscriber.objects.all()
-
-# RetrieveUpdateDestroyAPIView
-# used to GET, PUT, PATCH and DELETE.
-
-
+    # handles both the collection: /api/subscriber and element path /api/subscriber/1
+    # in urls: we can declare those paths separately
+    # SubscriberViewSet.as_view({'get': 'list', 'post': 'create'})     # For: /api/subscriber
+    # SubscriberViewSet.as_view({'get': 'retrieve', 'put': 'update'})  # For: /api/subscriber/1
+    # or we can use Router
