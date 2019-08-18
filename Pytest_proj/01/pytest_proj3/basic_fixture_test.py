33f11698586871987e056595ea886891fa589dcc
brb45 <sunusd@yahoo.com>
2019-08-18 10:10:17 -0700

    8/18/19 dft-1

--- Pytest_proj/01/pytest_proj3/basic_fixture_test.py
+++ Pytest_proj/01/pytest_proj3/basic_fixture_test.py
@@ -10,3 +10,12 @@ def test_comparewithBB(supply_AA_BB_CC):
 def test_comparewithCC(supply_AA_BB_CC):
     zz = 35
     assert supply_AA_BB_CC[2]==zz,"cc and zz comparison failed"
+
+# pytest.raise
+
+# input_arr = [100,'z', 100]
+# def test_with_exception(input_arr):
+#     if input_arr[1].is_alpha():
+#         raise TypeError("Please provide a number")
+#     return input_arr[1]
+
--- Pytest_proj/01/pytest_proj4/test_xfail_skip.py
+++ Pytest_proj/01/pytest_proj4/test_xfail_skip.py
@@ -1,22 +1,22 @@
 import pytest
 @pytest.mark.skip
-def test_add_1():
+def testadd_1():
 	assert 100+200 == 400,"failed"
 
 @pytest.mark.skip
-def test_add_2():
+def testadd_2():
 	assert 100+200 == 300,"failed"
 
 @pytest.mark.xfail
-def test_add_3():
+def testadd_3():
 	assert 15+13 == 28,"failed"
 
 @pytest.mark.xfail
-def test_add_4():
+def testadd_4():
 	assert 15+13 == 100,"failed"
 
-def test_add_5():
+def testadd_5():
 	assert 3+2 == 5,"failed"
 
-def test_add_6():
+def testadd_6():
 	assert 3+2 == 6,"failed"
